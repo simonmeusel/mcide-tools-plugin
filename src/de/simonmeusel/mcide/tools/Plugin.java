a81e50b84575bddbbf0864643d4093a139f74a15
Simon Meusel <simon.meusel.pc@t-online.de>
2016-08-17 19:17:02 +0200

    Add cinematic command metadata

--- src/de/simonmeusel/mcide/tools/Plugin.java
+++ src/de/simonmeusel/mcide/tools/Plugin.java
@@ -2,6 +2,7 @@ package de.simonmeusel.mcide.tools;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.simonmeusel.mcide.tools.commands.GetCinematicCommand;
 import de.simonmeusel.mcide.tools.commands.GetCommandsCommand;
 
 public class Plugin extends JavaPlugin {
@@ -9,6 +10,7 @@ public class Plugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		
+		getCommand("getcinematic").setExecutor(new GetCinematicCommand(this));
 		getCommand("getcommands").setExecutor(new GetCommandsCommand(this));
 		
 	}
